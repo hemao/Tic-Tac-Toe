f7d7031424de2abf17f118525109893d03bc18d8
HemaO <hema.omprakash@gmail.com>
2020-07-06 12:06:58 -0400

    script.js - added logic to identify who clicked the tile

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let board = document.querySelector(".board");
+//intilaize player and turns
+currentPlayer = "red"
+redCounter = 0
+blueCounter = 0
+
+//Draw board
+let board = document.querySelector(".board")
 for(let i=0;i<9; i++){
-    let tile = document.createElement("div");
-    tile.classList.add("tile");
+    let tile = document.createElement("div")
+    tile.classList.add("tile")
     tile.addEventListener("click",selectTile)
-    board.appendChild(tile);
+    board.appendChild(tile)
+}
+//color tile
+function selectTile(){
+    if(currentPlayer == red){
+        alert("YOU CLICKED ME!" + currentPlayer)
+        currentPlayer = "blue"
+    }else {
+        alert("YOU CLICKED ME!" + currentPlayer)
+        currentPlayer = "blue"
+    }
 }
 
-function selectTile(){
-    alert("YOU CLICKED ME!");
-}
+
+
